10d77c59d5208de060b5c1c04b66a7d1fd8c8aa6
faigler <faigler@post.bgu.ac.il>
2026-01-15 10:00:52 +0200

    complete main() in StompServer

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -1,8 +1,34 @@
 package bgu.spl.net.impl.stomp;
+import bgu.spl.net.api.MessageEncoderDecoder;
+import bgu.spl.net.api.MessagingProtocol;
+import bgu.spl.net.srv.Server;
 
 public class StompServer {
 
     public static void main(String[] args) {
-        // TODO: implement this
+        if(args.length < 2){
+            System.out.println("should get exactly 2 args : <port> <tpc/reactor>");
+            System.exit(1);
+        }
+        int port = Integer.parseInt(args[0]);
+        String serverType = args[1];
+
+        Server<String> server;
+
+        if(serverType.equals("tpc")){
+            server = Server.threadPerClient(
+                port, 
+                () -> (MessagingProtocol<String>) new StompMessagingProtocolImpl(), 
+                () -> (MessageEncoderDecoder<String>) new StompEncoderDecoder()
+                );
+            server.serve();
+        } else if(serverType.equals("reactor")){
+           final int NUM_THREADS = 100;
+           server =  Server.reactor(NUM_THREADS,
+             port,
+              () -> (MessagingProtocol<String>) new StompMessagingProtocolImpl(),
+               () -> (MessageEncoderDecoder<String>) new StompEncoderDecoder());
+               server.serve();
+        }
     }
 }
